7da3ca5225444cf5d58dc2612d5b87a3de0b9329
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-02-24 15:31:09 -0500

    Adicion de dependencia TailwindsCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-      <h1>Titulo</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Titulo</h1>
+        </>
+    )
 }
 
 export default App
