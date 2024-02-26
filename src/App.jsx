069b595a109f74d92e6ce27964509205c4da75d4
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-02-26 18:43:59 -0500

    agregando archivos adicionales

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-
 import Navbar from './components/common/header/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/common/footer/Footer'
 
 function App() {
-
-  return (
-    <>
-      <h1>Titulo</h1>
-      <div
-      >
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </div>
-    </>
-  )
-
+    return (
+        <>
+            <div>
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 export default App
