ea15fb3582067feaf194dea134079ab0ab6a5479
pablo-ambrogio <pabloambrogioc@gmail.com>
2024-02-27 15:19:30 -0300

    Se creo la pagina de admin-home

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home.jsx'
 import Vehicles from './components/pages/Vehicles.jsx'
+import AdminHome from './components/pages/admin/AdminHome.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<App />}>
           <Route path='/' element={<Home />} />
           <Route path='/vehicles' element={<Vehicles />} />
+          <Route path='/admin-home' element={<AdminHome />} />
         </Route>
       </Routes>
     </BrowserRouter>
