0646ae8989b1e88ad4875261a8b5896215d34f56
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-02-27 00:56:13 -0500

    ajustes a home con botones

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import { Outlet } from 'react-router-dom'
 import Footer from './components/common/footer/Footer'
 
 function App() {
-<<<<<<< HEAD
-
-  return (
-    <>
-      <div>
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </div>
-    </>
-  )
-
+    return (
+        <>
+            <div>
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 export default App
