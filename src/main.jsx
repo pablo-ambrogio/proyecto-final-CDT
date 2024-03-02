2c10ed8c620eb5137b77c5501dc4975456cbdfea
pablo-ambrogio <pabloambrogioc@gmail.com>
2024-03-01 22:59:21 -0300

    cambios en main

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Vehicles from './components/pages/Vehicles.jsx'
 import AdminHome from './components/pages/admin/AdminHome.jsx'
 import AdminVehiclesList from './components/pages/admin/AdminVehiclesList.jsx'
 import DetailsCar from './components/pages/DetailsCar.jsx'
+import Login from './components/pages/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,7 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
         <Route path='/' element={<App />}>
           <Route path='/' element={<Vehicles />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/vehicles/:id' element={<DetailsCar />} />
+
         </Route>
 
       </Routes>
