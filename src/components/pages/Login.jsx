779199b7c193e1bd8de283b486927da228d9529b
pablo-ambrogio <pabloambrogioc@gmail.com>
2024-03-07 23:46:46 -0300

    arreglos en login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -3,10 +3,10 @@ import LogoShort from '../../assets/Logo_Short 2.svg'
 const Login = () => {
     return (
         <>
-            <section className="w-full m-auto bg-blue h-full">
-                <div className="grid grid-cols-2 w-3/5 h-96 mx-auto bg-grey p-2 rounded-lg">
-                    <div className='grid col-span-1 text-center place-items-center'>
-                        <img src={LogoShort} alt="Logo AutoGo" className="w-3/5 mx-auto" />
+            <section className="w-full m-auto h-full flex items-center py-5">
+                <div className="grid grid-cols-2 w-3/5 h-full 2xl:h-3/5 mx-auto bg-grey p-2 rounded-lg">
+                    <div className='grid col-span-1 text-center place-items-center h-full'>
+                        <img src={LogoShort} alt="Logo AutoGo" className="xl:w-1/2 lg:w-3/5 mx-auto" />
                     </div>
                     <div className="grid col-span-1 bg-red place-items-center">
                         <div className='w-4/5'>
