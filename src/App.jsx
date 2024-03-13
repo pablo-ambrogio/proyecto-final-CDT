04921669f024d40cc2680ec094626d2b28bc057a
pablo-ambrogio <pabloambrogioc@gmail.com>
2024-03-13 14:51:07 -0300

    MERGE: arreglos en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,20 @@ import Footer from './components/common/footer/Footer'
 import Navbar from './components/common/header/Navbar'
 
 function App() {
-<<<<<<< HEAD
-    return (
-        <>
-            <div
-                // className='h-full bg-white'
-                className="grid min-h-screen grid-rows-[150px,1fr,70px]"
-            >
-                <Navbar />
-                <main className="mx-auto w-full">
-                    <Outlet />
-                </main>
-                <Footer />
-            </div>
-        </>
-    )
-=======
-
   return (
     <>
       <div
-        className='h-full bg-white'
+        className="grid min-h-screen grid-rows-[150px,1fr,70px]"
       >
-        <main className='mx-auto w-full'>
+        <Navbar />
+        <main className="mx-auto w-full">
           <Outlet />
         </main>
         <Footer />
       </div>
     </>
   )
->>>>>>> pablo
+
 }
 
 export default App
