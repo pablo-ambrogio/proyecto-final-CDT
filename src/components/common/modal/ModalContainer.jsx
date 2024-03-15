9a66499b23c9233d1deb94924c4c39aa43689d41
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-03-15 17:36:28 -0500

    se ajusto cambio nosotros

--- src/components/common/modal/ModalContainer.jsx
+++ src/components/common/modal/ModalContainer.jsx
@@ -6,17 +6,13 @@ const ModalContainer = ({ children, state, setState }) => {
         <>
             {state && (
                 <div className="w-screen h-screen fixed top-0 left-0 bg-grey bg-opacity-90 flex justify-center items-center p-6">
-                    <div className="w-[500px] min-h-[100px] relative rounded-md p-0">
+                    <div className="w-100 min-h-[100px] relative rounded-md p-0">
                         <button
                             className="absolute top-[-30px] right-[-30px] w-5 h-5 cursor-pointer border-0 ease-in-out duration-300 rounded text-blue font-bold
                                 hover:text-blue"
                             onClick={() => setState(false)}
                         >
-                            <FontAwesomeIcon
-                                icon={faX}
-                                className=""
-                            />
-                            
+                            <FontAwesomeIcon icon={faX} className="text-4xl" />
                         </button>
                         {children}
                     </div>
--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,61 +1,81 @@
-import integrante1 from '../../../src/assets/integrantes/Agus.png'
-import integrante2 from '../../../src/assets/integrantes/Andre.png'
-import integrante3 from '../../../src/assets/integrantes/Dennis.png'
-import integrante4 from '../../../src/assets/integrantes/Ivan.png'
-import integrante5 from '../../../src/assets/integrantes/JuanK.png'
+import integrante1 from '../../../src/assets/integrantes/Andre.png'
+import integrante2 from '../../../src/assets/integrantes/Mauro.png'
+import integrante3 from '../../../src/assets/integrantes/Pablo.png'
+import integrante4 from '../../../src/assets/integrantes/Mervin.png'
+import integrante5 from '../../../src/assets/integrantes/Ivan.png'
 import integrante6 from '../../../src/assets/integrantes/Kike.png'
-import integrante7 from '../../../src/assets/integrantes/Mauro.png'
-import integrante8 from '../../../src/assets/integrantes/Maye.png'
-import integrante9 from '../../../src/assets/integrantes/Mervin.png'
-import integrante10 from '../../../src/assets/integrantes/Pablo.png'
+import integrante7 from '../../../src/assets/integrantes/JuanK.png'
+import integrante8 from '../../../src/assets/integrantes/Dennis.png'
+import integrante9 from '../../../src/assets/integrantes/Agus.png'
+import integrante10 from '../../../src/assets/integrantes/Maye.png'
 import integrante11 from '../../../src/assets/integrantes/Rommy.png'
-import car from '../../../src/assets/Car3 1.svg'
 
 const AboutUs = () => {
     return (
         <>
-            <main className="h-full">
-                <h1 className="ml-10 text-blue text-2xl uppercase font-modern p-2">sobre nosotros</h1>
-                <div className="grid place-items-center grid-cols-3 gap-4 items-end my-10">
-                    <section className="bg-grey rounded-lg col-span-2">
-                        <div className="grid grid-cols-6 gap-4 p-2 w-5/6">
-                            <img src={integrante1} alt="integrante1" />
-                            <img src={integrante2} alt="integrante2" />
-                            <img src={integrante3} alt="integrante3" />
-                            <img src={integrante4} alt="integrante4" />
-                            <img src={integrante5} alt="integrante5" />
-                            <img src={integrante6} alt="integrante6" />
-                            <img src={integrante7} alt="integrante7" />
-                            <img src={integrante8} alt="integrante8" />
-                            <img src={integrante9} alt="integrante9" />
-                            <img src={integrante10} alt="integrante10" />
-                            <img src={integrante11} alt="integrante11" />
-                        </div>
-                        <div className="text-blue p-2 text-sm">
-                            <p>
-                                <span className="font-modern">Scrum Master:</span> Andrea de Oña
-                            </p>
-                            <p>
-                                <span className="font-modern">Frontend: </span> Mauricio Henao, Pablo Ambrogio
-                            </p>
-                            <p>
-                                <span className="font-modern">Backend:</span> Juan Carlos Álvarez, Agustin Rios, Dennis Juilland
-                            </p>
-                            <p>
-                                <span className="font-modern">Base de Datos:</span> Mayerline Bueno, Romina Mastropierro
-                            </p>
-                            <p>
-                                <span className="font-modern">Infraestructura:</span> Mervin Sosa
-                            </p>
-                            <p>
-                                <span className="font-modern">Testing:</span> Ivan Gómez
-                            </p>
-                            <p>
-                                <span className="font-modern">UX/UI:</span> Enrique González
-                            </p>
-                        </div>
-                    </section>
-                    <img className="mr-[-32%]" src={car} alt="Car" />
+            <main className="flex flex-col">
+                <h1 className="px-10 text-blue text-2xl uppercase font-modern">
+                    nosotros
+                </h1>
+                {/* <div className="flex justify-center items-center p-10"> */}
+                <div className="grid grid-cols-1 p-10">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 bg-blue p-10 rounded-lg gap-2 ">
+                        <img
+                            src={integrante1}
+                            alt="integrante1"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante2}
+                            alt="integrante2"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante3}
+                            alt="integrante3"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante4}
+                            alt="integrante4"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante5}
+                            alt="integrante5"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante6}
+                            alt="integrante6"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante7}
+                            alt="integrante7"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante8}
+                            alt="integrante8"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante9}
+                            alt="integrante9"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante10}
+                            alt="integrante10"
+                            className="w-[100%]"
+                        />
+                        <img
+                            src={integrante11}
+                            alt="integrante11"
+                            className="w-[100%]"
+                        />
+                    </div>
                 </div>
             </main>
         </>
