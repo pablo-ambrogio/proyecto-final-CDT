69999d942e7e00caa0a7a40e7079d138bbc27ff9
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-03-21 14:58:09 -0500

    avances con buscador

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/common/footer/Footer'
-import Navbar from './components/common/header/Navbar'
+// import Navbar from './components/common/header/Navbar'
+import Header from './components/common/header/Header'
 
 function App() {
-  return (
-    <>
-      <div
-        className="grid min-h-screen grid-rows-[150px,1fr,70px]"
-      >
-        {/* <main className='mx-auto w-full'> */}
-        {/* <Outlet /> */}
-
-        <Navbar />
-        <section className="mx-auto w-full">
-          <Outlet />
-        </section>
-
-        <Footer />
-      </div>
-
-    </>
-  )
-
+    return (
+        <>
+            <div className="grid min-h-screen grid-rows-[150px,1fr,70px]">
+                <Header />
+                <section className="mx-auto w-full">
+                    <Outlet />
+                </section>
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/components/common/header/Navbar.jsx
+++ src/components/common/header/Navbar.jsx
@@ -1,119 +1,38 @@
-import LogoHeader from '../../../assets/LogoRojo 1.svg'
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
-import Swal from 'sweetalert2'
-import Modal from '../modal/Modal'
 
 const Navbar = () => {
     return (
         <>
-            <header className="flex flex-col sticky top-0">
-                <nav class="flex items-center justify-between flex-wrap bg-blue py-2 px-6">
-                    <Link
-                        to="/"
-                        className="flex items-center flex-shrink-0 mr-6 hover:transition-in-out duration-300 hover:scale-110 hover:transition-in-out"
-                    >
-                        <img src={LogoHeader} alt="Logo AutoGo" />
-                    </Link>
-                    <div class="block lg:hidden">
-                        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                            <FontAwesomeIcon
-                                icon={faBars}
-                                className="text-blue"
-                            />
-                        </button>
-                    </div>
-                    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                        <div class="text-md lg:flex-grow flex justify-center gap-24">
-                            <Link
-                                to="/"
-                                className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
-                            >
-                                vehículos
-                            </Link>
-                            <Link
-                                to="/offers"
-                                className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
-                                onClick={() =>
-                                    alert(
-                                        'Estamos trabajando para darte las mejores ofertas del mercado'
-                                    )
-                                }
-                                // onClick={() =>
-                                //     Swal.fire({
-                                //         confirmButtonColor: '#041562',
-                                //         html: '<p class = "text-red text-xl font-bold" >Estamos trabajando para darte las mejores ofertas del mercado</p>'
-                                //     })
-                                // }
-                            >
-                                ofertas
-                            </Link>
-                            <Link
-                                to="/offers"
-                                className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
-                                onClick={() =>
-                                    alert(
-                                        'Pronto podrás seleccionar tus opciones favoritas'
-                                    )
-                                }
-
-                                // onClick={() =>
-                                //     Swal.fire({
-                                //         confirmButtonColor: '#041562',
-                                //         html: '<p class = "text-red text-xl font-bold" >Estamos trabajando para darte las mejores ofertas del mercado</p>'
-                                //     })
-                                // }
-                            >
-                                favoritos
-                            </Link>
-                        </div>
-                        <div className="flex gap-2">
-                            {/* <Link
-                                to="/register"
-                                className="inline-block text-sm capitalize font-bold px-4 py-2 leading-none rounded-lg bg-red hover:bg-grey hover:text-blue transition-in-out duration-700 "
-                            >
-                                crear cuenta
-                            </Link>
-
-                            <Link
-                                to="/login"
-                                className="inline-block text-sm capitalize font-bold px-4 py-2 leading-none rounded-lg bg-red hover:bg-grey hover:text-blue transition-in-out duration-700 "
-                            >
-                                iniciar sesión
-                            </Link> */}
-                            <Modal />
-                        </div>
-                    </div>
-                </nav>
-                <section className="flex bg-blue2 w-full px-16 py-2 gap-3">
-                    <div className="flex flex-1 flex-col">
-                        <p>Ubicación para retirar el vehículo</p>
-                        <input
-                            className="rounded py-1 px-5 bg-grey text-blue outline-none "
-                            type="text"
-                            placeholder="Punto de retirada (Ej: Buenos Aires, Bogotá)"
-                        />
-                    </div>
-                    <div className="">
-                        <p>Fecha inicio</p>
-                        <input
-                            className="rounded py-1 px-5 bg-grey text-blue outline-none"
-                            type="date"
-                        />
-                    </div>
-                    <div className="">
-                        <p>Fecha fin</p>
-                        <input
-                            className="rounded py-1 px-5 bg-grey text-blue outline-none"
-                            type="date"
-                        />
-                    </div>
-                    <button className="block py-2 px-4 leading-none rounded-lg text-sm text-white border-0 bg-blue hover:bg-grey hover:text-blue cursor-pointer font-bold capitalize transition-in-out duration-700">
-                        Buscar
-                    </button>
-                </section>
-            </header>
+            <div class="text-md lg:flex-grow flex justify-center gap-24">
+                <Link
+                    to="/"
+                    className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
+                >
+                    vehículos
+                </Link>
+                <Link
+                    to="/offers"
+                    className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
+                    onClick={() =>
+                        alert(
+                            'Estamos trabajando para darte las mejores ofertas del mercado'
+                        )
+                    }
+                >
+                    ofertas
+                </Link>
+                <Link
+                    to="/offers"
+                    className="uppercase font-bold block mt-4 lg:inline-block lg:mt-0 text-grey hover:text-red over:transition-in-out duration-300 mr-4"
+                    onClick={() =>
+                        alert(
+                            'Pronto podrás seleccionar tus opciones favoritas'
+                        )
+                    }
+                >
+                    favoritos
+                </Link>
+            </div>
         </>
     )
 }
