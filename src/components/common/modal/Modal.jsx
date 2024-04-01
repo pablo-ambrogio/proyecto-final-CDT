966f62431074e7326ca6a28603fdc5020604ed58
MauricioHenaoRestrepo <mhenaor@gmail.com>
2024-04-01 08:36:39 -0500

    .

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -7,15 +7,10 @@ const ModalContainer = ({ children, state, setState }) => {
                 <div className="w-screen h-screen fixed top-0 left-0 bg-darkgrey bg-opacity-60 flex justify-center items-center p-6">
                     <div className="w-100 min-h-[100px] relative rounded-md p-0">
                         <button
-                            className="absolute top-[-50px] right-[-50px] w-7 h-15 cursor-pointer border-0 ease-in-out duration-300 rounded text-white font-bold
-                                hover:text-secondary"
+                            className="absolute top-7 right-7 w-9 h-15 cursor-pointer border-0 ease-in-out duration-300 rounded text-white font-bold
+                            hover:text-secondary"
                         >
-                            <img
-                                onClick={() => setState(false)}
-                                className="bg-grey rounded"
-                                src={Xmark}
-                                alt="cerrar"
-                            />
+                            <img onClick={() => setState(false)} className="bg-grey rounded" src={Xmark} alt="cerrar" />
                         </button>
                         {children}
                     </div>
